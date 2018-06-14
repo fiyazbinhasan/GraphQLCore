f306471087f6fd69470a80c69ce340840aebbac7
Fiyaz Hasan <fiyazhasan@live.com>
2018-06-14 17:55:16 +0600

    add caching for Items field

--- GraphQLAPI/InventoryQuery.cs
+++ GraphQLAPI/InventoryQuery.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using GraphQL.DataLoader;
 using GraphQL.Types;
 using GraphQLAPI;
 using GraphQLAPI.Models;
@@ -10,43 +11,44 @@ namespace GraphQLAPI
 {
     public class InventoryQuery : ObjectGraphType
     {
-        public InventoryQuery(IDataStore dataStore)
+        public InventoryQuery(IDataLoaderContextAccessor accessor, IDataStore dataStore)
         {
-			Field<ListGraphType<ItemType>, IEnumerable<Item>>()
-				.Name("Items")
-				.ResolveAsync(ctx =>
-				{
-					return dataStore.GetItemsAsync();
-				});
+            Field<ListGraphType<ItemType>, IEnumerable<Item>>()
+                .Name("Items")
+                .ResolveAsync(ctx =>
+                {
+                    var loader = accessor.Context.GetOrAddLoader("GetAllItems", () => dataStore.GetItemsAsync());
+                    return loader.LoadAsync();
+                });
 
-			Field<ItemType, Item>()
-				.Name("Item")
-				.Argument<NonNullGraphType<StringGraphType>>("barcode", "item barcode")
-				.ResolveAsync(ctx =>
-			    {
-				    var barcode = ctx.GetArgument<string>("barcode");
-				    return dataStore.GetItemByBarcodeAsync(barcode);
-			    });
+            Field<ItemType, Item>()
+                .Name("Item")
+                .Argument<NonNullGraphType<StringGraphType>>("barcode", "item barcode")
+                .ResolveAsync(ctx =>
+                {
+                    var barcode = ctx.GetArgument<string>("barcode");
+                    return dataStore.GetItemByBarcodeAsync(barcode);
+                });
 
-			Field<ListGraphType<OrderType>, IEnumerable<Order>>()
+            Field<ListGraphType<OrderType>, IEnumerable<Order>>()
                 .Name("Orders")
                 .ResolveAsync(ctx =>
                 {
-				    return dataStore.GetOrdersAsync();
+                    return dataStore.GetOrdersAsync();
                 });
 
-			Field<ListGraphType<CustomerType>, IEnumerable<Customer>>()
+            Field<ListGraphType<CustomerType>, IEnumerable<Customer>>()
                 .Name("Customers")
                 .ResolveAsync(ctx =>
                 {
                     return dataStore.GetCustomersAsync();
                 });
 
-			Field<ListGraphType<OrderItemType>, IEnumerable<OrderItem>>()
+            Field<ListGraphType<OrderItemType>, IEnumerable<OrderItem>>()
                 .Name("OrderItem")
                 .ResolveAsync(ctx =>
                 {
-				    return dataStore.GetOrderItemAsync();
+                    return dataStore.GetOrderItemAsync();
                 });
         }
     }
